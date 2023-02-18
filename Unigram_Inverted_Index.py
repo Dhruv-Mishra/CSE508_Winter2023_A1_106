9f9ecd7a4244374de36af235f9c7247753f18b8f
HunarKaur25 <88452809+HunarKaur25@users.noreply.github.com>
2023-02-18 08:34:26 +0530

    Patched and Standalone code of Unigram Inverted Index

--- Unigram_Inverted_Index.py
+++ Unigram_Inverted_Index.py
@@ -40,23 +40,64 @@ class Query:
     def getQuery(self):
         return self.input_li,self.op_li
 
-class Inverted_index:
+class Inverted_Index:
     inverted_ind = {}
     universal_set = set()
     comparisons = 0
+    document_count = 0
+    name_arr = []
 
     def __init__(self):
         self.inverted_ind = {}
         self.universal_set = set()
         self.comparisons = 0
+        self.document_count = 0
+        self.name_arr = []
+       
+    def extract_text(self,s):
+        start = [0,0] #Start is inclusive 
+        end = [0,0] #End is exclusive
+        for i in range(len(s)):
+            if(i + 7 <= len(s) and s[i:i+7] == "<TITLE>"):
+                start[0] = i+7
+            elif(i + 8 <= len(s) and s[i:i+8] == "</TITLE>"):
+                end[0] = i
+            elif(i + 6 <= len(s) and s[i:i+6] == "<TEXT>"):
+                start[1] = i+6
+            elif(i + 7 <= len(s) and s[i:i+7] == "</TEXT>"):
+                end[1] = i
+        new_s = s[start[0]:end[0]] + " " + s[start[1]:end[1]]
+        new_s = " ".join(new_s.split("\n"))
+        new_s = " ".join(new_s.split("-"))
+        return new_s
     
-    def addDoc(self,key,id):
-        if(key in self.inverted_ind.keys()):
-            self.inverted_ind[str(key)].append(int(id))
-        else:
-            li = [int(id)]
-            self.inverted_ind[str(key)] = li
-        self.universal_set.add(int(id))
+    def addDoc(self,token_list,id): #token list in order, id of parent document 
+        for i in range(len(token_list)):
+            key = token_list[i]
+            if(key in self.inverted_ind.keys()):
+                self.inverted_ind[str(key)].append(int(id))
+            else:
+                li = [int(id)]
+                self.inverted_ind[str(key)] = li
+            self.universal_set.add(int(id))
+                
+    def new_Data(self,path):
+        f = open(path,"r")
+        s = f.read()
+        new_s = self.extract_text(s)
+        new_s = new_s.lower()
+        translate_table = dict((ord(char), " ") for char in string.punctuation)   
+        new_s = new_s.translate(translate_table)
+        f.close()
+        li = word_tokenize(new_s)
+        stop_words = set(stopwords.words("english"))
+        filter_li = []
+        for words in li:
+            if(words not in stop_words):
+                filter_li.append(words)
+        self.addDoc(filter_li,self.document_count)
+        self.name_arr.append(path)
+        self.document_count+=1
 
     def showWord(self,key):
         return self.inverted_ind[str(key)]
@@ -79,13 +120,18 @@ class Inverted_index:
                     not_count = 1
                 else:
                     simplified_seq.append(op_seq[i])
-        simplified_seq.append(op_seq[len(op_seq)-1])
+        if(op_seq[len(op_seq)-1] == "not"):
+            if(not_count%2 == 1):
+                simplified_seq.append(op_seq[len(op_seq)-1])
+        else:
+            simplified_seq.append(op_seq[len(op_seq)-1])
         return simplified_seq
     
     def processQuery(self,input_seq,op_seq):
         self.comparisons = 0
         query = Query(input_seq,op_seq)
         input_li,op_li = query.getQuery()
+        og_qry = self.getStringQuery(input_li,op_li)
         op_li = self.simplify_not(op_li)
         str_qry = self.getStringQuery(input_li,op_li)
         for i in range(len(input_li)):
@@ -96,7 +142,7 @@ class Inverted_index:
         output = self.query_sched(input_li,op_li)
         comp_ans = self.comparisons
         self.comparisons = 0
-        return output,comp_ans,str_qry
+        return output,comp_ans,str_qry,og_qry
     
     def getStringQuery(self,input_li,op_li):
         ans = []
@@ -117,10 +163,16 @@ class Inverted_index:
         return final_ans
 
     def getOutput(self,input_seq,op_seq):
-        output,comp_ans,str_qry = self.processQuery(input_seq,op_seq)
-        print("Input Query:",str_qry)
+        output,comp_ans,str_qry,og_qry = self.processQuery(input_seq,op_seq)
+        print("Original Input Query:",og_qry)
+        print("Simplified Input Query:",str_qry)
         print("Number of Documents:",len(output[0]))
-        print("Document IDs:",*output[0])
+        if(len(output[0])>0):
+            print("Document Names:")
+            for j in range(len(output[0])):
+                print("\t",str(j+1)+".",self.name_arr[output[0][j]])
+        else:
+            print("No documents to show!")
         print("Number of comparisons for fetching result:",comp_ans)
 
     
@@ -189,74 +241,44 @@ class Inverted_index:
                 output.append(b[bit])
                 bit+=1
             self.comparisons+=1
-        while(a[ait] < len(a_list)):
+        while(ait < len(a_list)):
             output.append(a[ait])
             ait+=1
-        while(b[bit] < len(b_list)):
+        while(bit < len(b_list)):
             output.append(b[bit])
             bit+=1
         return output 
     
     def query_not(self,a):
-        a_set = set(a)
-        output = list(self.universal_set-a_set)
+        univ = list(self.universal_set)
+        univ.sort()
+        output = []
+        self.comparisons = 0
+        ait = 0
+        for i in range(len(univ)):
+            if(ait < len(a)):
+                self.comparisons+=1
+            if(ait < len(a) and univ[i] == a[ait]):
+                ait+=1
+            else:
+                output.append(univ[i])
         return output
-
-
-def extract_text(s):
-    start = [0,0] #Start is inclusive 
-    end = [0,0] #End is exclusive
-
-    for i in range(len(s)):
-        if(i + 7 <= len(s) and s[i:i+7] == "<TITLE>"):
-            start[0] = i+7
-        elif(i + 8 <= len(s) and s[i:i+8] == "</TITLE>"):
-            end[0] = i
-        elif(i + 6 <= len(s) and s[i:i+6] == "<TEXT>"):
-            start[1] = i+6
-        elif(i + 7 <= len(s) and s[i:i+7] == "</TEXT>"):
-            end[1] = i
-
-    new_s = s[start[0]:end[0]] + " " + s[start[1]:end[1]]
-    new_s = " ".join(new_s.split("\n"))
-    new_s = " ".join(new_s.split("-"))
-
-    return new_s
 
 def getNum(i):
     return (4-len(str(i)))*"0" + str(i)
 
-inverted_index = Inverted_index()
+try:
+    inverted_index = pickle.load(open("unigram_inverted_index_savefile.pickle", "rb"))
+except (OSError, IOError) as e:
+    inverted_index = Inverted_Index()
+    for j in range(1,1401): # 1,1401
+        f = inverted_index.new_Data("CSE508_Winter2023_Dataset/cranfield"+getNum(j))
+    pickle.dump(inverted_index, open("unigram_inverted_index_savefile.pickle", "wb"))
 
-for j in range(1,10): # 1,1401
-    f = open("CSE508_Winter2023_Dataset/cranfield"+getNum(j),"r")
-    s = f.read()
-    #print("Original File:",s)
-    new_s = extract_text(s)
-    #print("Baisc Text Extraction:",new_s)
-    new_s = new_s.lower()
-    #print("Converting to Lowercase:",new_s)
-    translate_table = dict((ord(char), " ") for char in string.punctuation)   
-    new_s = new_s.translate(translate_table)
-    #print("After Removal of Punctuation:",new_s)
-    f.close()
-    li = word_tokenize(new_s)
-    #print("Tokenizing the string:",end = " ")
-    #print(li)
-    stop_words = set(stopwords.words("english"))
-    filter_li = []
-    for words in li:
-        if(words not in stop_words):
-            filter_li.append(words)
-    #print("Filtering Stopwords:",filter_li)
-    filter_li = list(set(filter_li))
-    for token in filter_li:
-        inverted_index.addDoc(token,getNum(j))
-
+print("\n++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
 n = int(input())
 for i in range(n):
     inp_seq = input()
-    op_seq = input()
+    op_seq = input() 
     output = inverted_index.getOutput(inp_seq,op_seq)
-with open('file.pkl', 'wb') as file:
-    pickle.dump(inverted_index,file)
+    print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
